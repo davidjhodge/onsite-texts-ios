f6ada942c903e7354122393248338ed51ea1d154
davidjhodge <dhodge@genesisappsonline.com>
2015-08-12 10:39:52 -0400

    Swift project replaced with Objective-C.

---
+++ OnsiteTexts/OnsiteTexts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnsiteTexts
+//
+//  Created by David Hodge on 8/12/15.
+//  Copyright (c) 2015 Genesis Apps, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OnsiteTexts/OnsiteTexts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OnsiteTexts
+//
+//  Created by David Hodge on 8/12/15.
+//  Copyright (c) 2015 Genesis Apps, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
