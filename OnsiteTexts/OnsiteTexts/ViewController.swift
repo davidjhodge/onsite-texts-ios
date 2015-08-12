297f58512c34aa8e8cddc0da89f19e9aa56f29d6
davidjhodge <dhodge@genesisappsonline.com>
2015-08-12 10:30:23 -0400

    Initial Project Commit. Pods installed.

---
+++ OnsiteTexts/OnsiteTexts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnsiteTexts
+//
+//  Created by David Hodge on 8/12/15.
+//  Copyright (c) 2015 Genesis Apps, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OnsiteTexts/Pods/Target Support Files/Pods-OnsiteTexts/Pods-OnsiteTexts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OnsiteTexts : NSObject
+@end
+@implementation PodsDummy_Pods_OnsiteTexts
+@end
